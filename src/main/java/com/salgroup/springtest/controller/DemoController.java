94d28f47fd4a7a4b108a2de55c9b4c216e335e5c
SALISH PUTHENVEETTIL SALIM <salish.ps@gmail.com>
2022-10-24 01:30:27 -0700

    adding files

---
+++ src/main/java/com/salgroup/springtest/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.salgroup.springtest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping(value = "/saldemo", method = RequestMethod.GET)
+	public String fun(@RequestParam("userName") String userName) {
+		System.out.println("In controller");
+		return "automation testing here " + userName;
+	}
+
+}
